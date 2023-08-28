6e0a65d87ab35db0132bafde884bea988d9399a3
MoazSalem <moaztalaat6@gmail.com>
2023-08-28 14:16:41 +0300

    add animation to splash view ui

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -2,15 +2,45 @@ import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
+    controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset(AssetsData.logo, height: 50, semanticsLabel: 'A red up arrow'),
-      ),
+      body: AnimatedBuilder(
+          animation: animation,
+          builder: (context, _) {
+            return FadeTransition(
+              opacity: animation,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SvgPicture.asset(AssetsData.logo, height: 50, semanticsLabel: 'bookly logo'),
+                  const SizedBox(height: 10),
+                  Text('Read Free Books',
+                      style: Theme.of(context).textTheme.bodyMedium, textAlign: TextAlign.center),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
